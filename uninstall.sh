9fd01809ce7ac39b17d8fb37202aa901710ac561
YUDAMODS <119145280+YUDAMODS@users.noreply.github.com>
2024-09-10 20:48:29 +0700

    Add files via upload

---
+++ uninstall.sh
@@ -0,0 +1,21 @@
+# [+] =============================================== [+]
+# [+]                                                 [+]
+# [+] - INSTALLER PTERODACTYL                         [+]
+# [+]                                                 [+]
+# [+]  TQTO :                                         [+]
+# [+] • YUDAMODS ( MySelf)                            [+]
+# [+] • Creator Bot WhatsApp & Telegram               [+]
+# [+] • Para Pengguna Bot Tele & Wa                   [+]
+# [+]                                                 [+]
+# [+] © CreateByYudaMods                               [+]
+# [+] =============================================== [+]
+
+# Membuat lokasi baru
+bash <(curl -s https://pterodactyl-installer.se) <<EOF
+y
+y
+y
+y
+EOF
+
+echo "Proses Uninstall Panell Selesai."
